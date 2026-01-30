d4e48ad7a44881053a3719325c15bea7888a6543
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2026-01-30 18:31:07 +0100

    Add managers

---
+++ Assets/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Namen der zu ladenden Szenen")]
+    public string sceneName1;
+    public string sceneName2;
+
+    void Start()
+    {
+        LoadExtraScenes();
+    }
+
+    void LoadExtraScenes()
+    {
+        // Prüfen, ob Szene 1 bereits geladen ist, um Duplikate zu vermeiden
+        if (!SceneManager.GetSceneByName(sceneName1).isLoaded)
+        {
+            SceneManager.LoadSceneAsync(sceneName1, LoadSceneMode.Additive);
+        }
+
+        // Prüfen für Szene 2
+        if (!SceneManager.GetSceneByName(sceneName2).isLoaded)
+        {
+            SceneManager.LoadSceneAsync(sceneName2, LoadSceneMode.Additive);
+        }
+    }
+}
