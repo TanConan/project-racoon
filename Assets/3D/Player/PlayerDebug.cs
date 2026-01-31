f5dc61a7b07aab733f1911a735f2d66ad3b00397
Gragon <gragonator02@gmail.com>
2026-01-31 01:18:47 +0100

    Fixed triple trigger of debug methods

--- Assets/3D/Player/PlayerDebug.cs
+++ Assets/3D/Player/PlayerDebug.cs
@@ -3,32 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerDebug : MonoBehaviour, InputSystem.IDebugActions
 {
-    public Mask mask;
+  public Mask mask;
 
-    private InputSystem inputSystem;
-    private InputSystem.DebugActions debugActions;
+  private InputSystem inputSystem;
+  private InputSystem.DebugActions debugActions;
 
-    void Awake()
-    {
-        inputSystem = new();
-        debugActions = inputSystem.Debug;
-        debugActions.AddCallbacks(this);
-        debugActions.Enable();
-    }
+  void Awake()
+  {
+    inputSystem = new();
+    debugActions = inputSystem.Debug;
+    debugActions.AddCallbacks(this);
+    debugActions.Enable();
+  }
 
-    public void OnMask0(InputAction.CallbackContext context)
-    {
-        mask.ChangeMask(ActiveMasks.NONE);
-    }
+  public void OnMask0(InputAction.CallbackContext context)
+  {
+    if (!context.performed) { return; }
+    mask.ChangeMask(ActiveMasks.NONE);
+  }
 
-    public void OnMask1(InputAction.CallbackContext context)
-    {
-        mask.ChangeMask(mask.SelectedActiveMasks ^ ActiveMasks.RedBlueMask);
-    }
+  public void OnMask1(InputAction.CallbackContext context)
+  {
+    if (!context.performed) { return; }
+    mask.ChangeMask(mask.SelectedActiveMasks ^ ActiveMasks.RedBlueMask);
+  }
 
-    public void OnMask2(InputAction.CallbackContext context)
-    {
-        mask.ChangeMask(mask.SelectedActiveMasks ^ ActiveMasks.FIND);
-    }
+  public void OnMask2(InputAction.CallbackContext context)
+  {
+    if (!context.performed) { return; }
+    mask.ChangeMask(mask.SelectedActiveMasks ^ ActiveMasks.FIND);
+  }
 
 }
