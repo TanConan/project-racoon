36c385e192846836f20a9b8161fa88f6ff5daaba
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2026-02-01 22:02:03 +0100

    After publish fix

--- Assets/2D/Peter/Death.cs
+++ Assets/2D/Peter/Death.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
-  public Soundboard2D soundboard2D;
-  public bool isDying;
-  private Animator animator;
+    public bool isDying;
+    private Animator animator;
+    private Soundboard2D soundboard2D;
 
-  private void Awake()
-  {
-    animator = GetComponent<Animator>();
-  }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        soundboard2D = FindAnyObjectByType<Soundboard2D>();
+    }
 
-  public void Die()
-  {
-    if (isDying) return;
-    isDying = true;
-    soundboard2D.PlaySound("Death");
-    Array.ForEach(FindObjectsByType<GridMovement>(FindObjectsSortMode.None), gm => gm.isMovementActive = false);
-    LevelManager.Instance.ReloadLevel();
-  }
+    public void Die()
+    {
+        if (isDying) return;
+        isDying = true;
+        soundboard2D.PlaySound("Death");
+        Array.ForEach(FindObjectsByType<GridMovement>(FindObjectsSortMode.None), gm => gm.isMovementActive = false);
+        LevelManager.Instance.ReloadLevel();
+    }
 
-  public void FallDie()
-  {
-    if (isDying) return;
-    isDying = true;
-    Array.ForEach(FindObjectsByType<GridMovement>(FindObjectsSortMode.None), gm => gm.isMovementActive = false);
-    animator.SetTrigger("fall");
-    StartCoroutine(DeathRoutine());
-  }
+    public void FallDie()
+    {
+        if (isDying) return;
+        isDying = true;
+        Array.ForEach(FindObjectsByType<GridMovement>(FindObjectsSortMode.None), gm => gm.isMovementActive = false);
+        animator.SetTrigger("fall");
+        StartCoroutine(DeathRoutine());
+    }
 
-  private IEnumerator DeathRoutine()
-  {
-    yield return new WaitForSeconds(2f);
-    soundboard2D.PlaySound("Death");
-    yield return new WaitForSeconds(1f);
-    LevelManager.Instance.ReloadLevel();
-  }
+    private IEnumerator DeathRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+        soundboard2D.PlaySound("Death");
+        yield return new WaitForSeconds(1f);
+        LevelManager.Instance.ReloadLevel();
+    }
 }
